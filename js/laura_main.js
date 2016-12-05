aea97ae9dce3ba182795098cb88a898f5c3ad685
LauraMo <lauvimopo@gmail.com>
2016-12-05 12:32:09 -0500

    add phoneTest and change title on index file

--- js/laura_main.js
+++ js/laura_main.js
@@ -1,9 +1,22 @@
+// Carte mon expérience
+
 var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
-    }
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: -34.397,
+            lng: 150.644
+        },
+        zoom: 8
+    });
+}
 
 console.log("connectée si señora");
+
+// Effets textos
+
+
+AOS.init({
+    duration: 1200
+});
